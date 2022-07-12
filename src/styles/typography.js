f89348381b9c2fee343206eabcd6c2fd71fa7674
Lucho Zelada <luisdanger96@gmail.com>
2022-07-12 16:34:08 -0500

    ADD: typography styles

---
+++ src/styles/typography.js
@@ -0,0 +1,94 @@
+export const typography= {
+  headline: {
+    h1: `
+      font-weight: 300;
+      font-size: 97px;
+      line-height: 118px;
+    `,
+
+    h2: `
+      font-weight: 300;
+      font-size: 61px;
+      line-height: 74px;
+    `,
+
+    h3: `
+      font-weight: 400;
+      font-size: 48px;
+      line-height: 59px;
+    `,
+
+    h4: `
+      font-weight: 400;
+      font-size: 34px;
+      line-height: 41px;
+      letter-spacing: 0.25px;
+    `,
+
+    h5: `
+      font-weight: 400;
+      font-size: 24px;
+      line-height: 29px;
+    `,
+
+    h6: `
+      font-weight: 500;
+      font-size: 20px;
+      line-height: 28px; 
+      letter-spacing: 0.15px;  
+    `
+  },
+
+  subtitle: {
+    s1: `
+      font-weight: 400;
+      font-size: 16px;
+      line-height: 20px;
+      letter-spacing: 0.15px;
+    `,
+
+    s2: `
+      font-weight: 500;
+      font-size: 14px;
+      line-height: 18px;
+      letter-spacing: 0.1px;
+    ` 
+  },
+
+  body: {
+    b1: `
+      font-weight: 400;
+      font-size: 16px;
+      line-height: 24px;
+      letter-spacing: 0.5px;
+    `,
+
+    b2: `
+      font-weight: 400;
+      font-size: 14px;
+      line-height: 20px;
+      letter-spacing: 0.25px;
+    `
+  },
+
+  button: `
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 24px;
+    letter-spacing: 1.25px;
+  `,
+
+  caption: `
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 16px;
+    letter-spacing: 0.4px;
+  `,
+
+  overline: `
+    font-weight: 400;
+    font-size: 10px;
+    line-height: 16px;
+    letter-spacing: 1.5px;
+  ` 
+} 
