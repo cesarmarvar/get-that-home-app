fdc7b679e46d25bb7004fdcbfb9e08cbd2ff996c
Cesar Martinez <cmarvarc@hotmail.com>
2022-07-12 17:13:28 -0500

    Added adress and bottom icons to card

--- src/components/card.js
+++ src/components/card.js
@@ -1,25 +1,32 @@
 import styled from "@emotion/styled";
 import { RiMoneyDollarCircleLine } from "react-icons/ri";
 import { RiBuildingLine } from "react-icons/ri";
+import { BiBed } from "react-icons/bi";
+import { BiBath } from "react-icons/bi";
 
 const DefaultCard = styled.div`
-width: 300px;
-height: 360px;
+  width: 300px;
+  height: 360px;
 `
 
 const PropertyData = styled.div`
-display: flex;
-flex-direction: column;
-height: 160px;
+  display: flex;
+  flex-direction: column;
+  height: 160px;
 `
 
 const PriceRow = styled.div`
-display: flex;
-flex-direction: row;
-align-items: center;
-gap: 10px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 10px;
 `
 
+const IconsRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+`
 
 export function Card() {
 
@@ -29,13 +36,20 @@ export function Card() {
       <div>photo</div>
       <PropertyData>
         <PriceRow>
-          <RiMoneyDollarCircleLine />
-          <text>3,000</text>
+          <div>
+            <RiMoneyDollarCircleLine />
+            <text>3,000</text>
+          </div>
+          <div>
+            <RiBuildingLine />
+            <text>Apartment</text>
+          </div>
         </PriceRow>
-        <div>
-          <RiBuildingLine />
-          <text>Apartment</text>
-        </div>
+        <text>Adress</text>
+        <IconsRow>
+          <BiBed />
+
+        </IconsRow>
       </PropertyData>
 
     </DefaultCard>
