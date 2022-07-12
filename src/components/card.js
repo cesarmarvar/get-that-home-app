81c8e541394f76382cb66dab00722bca3561f7a7
Cesar Martinez <cmarvarc@hotmail.com>
2022-07-12 18:17:21 -0500

    Added bordered bottom with color to default card

--- src/components/card.js
+++ src/components/card.js
@@ -9,9 +9,16 @@ import { colors } from "../styles/colors";
 import { typography } from "../styles/typography";
 import { fonts } from "../styles/fonts";
 
+const CardContainer = styled.div`
+
+`
+
 const DefaultCard = styled.div`
   width: 300px;
   height: 360px;
+  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.2);
+  border-top-right-radius: 8px;
+  border-top-left-radius: 8px;
 `
 
 const PropertyImg = styled.div`
@@ -70,51 +77,68 @@ const Adress = styled.text`
 const IconsRow = styled.div`
   display: flex;
   flex-direction: row;
-  gap: 8px;
+  gap: 1rem;
+  align-items: center;
+  color: ${colors.gray.regular};
+  ${typography.body.b1};
+  font-family: ${fonts.primary}
 `
 
 const SingleIcon = styled.div`
-  display: flex;  
-  flex-directin: row;
+  display: flex;
+  align-items: center;
   gap: 4px;
 `
+
+const ColorBottom = styled.div`
+  width: 300px;
+  height: 7px;
+  background-color: ${colors.pink.dark};
+  border-bottom-left-radius: 6px;
+  border-bottom-right-radius: 6px;
+`
+
+// style={{border: "2px solid brown"}} ====> debug
 
 export function Card() {
 
 
   return(
-    <DefaultCard style={{border: "2px solid brown"}} >
-      <PropertyImg style={{border: "2px solid brown"}}>photo</PropertyImg>
-      <PropertyData>
-        <PriceRow>
-          <Price style={{width: "161px"}}>
-            <RiMoneyDollarCircleLine size="30px" color={`${colors.gray.dark}`}/>
-            <text>3,000</text>
-          </Price>
-          <PropertyType>
-            <RiBuildingLine size="30px" color={`${colors.gray.light}`}/>
-            <text>Apartment</text>
-          </PropertyType>
-        </PriceRow>
-        <AddressAndIcons>
-          <Adress>86872 Jacob Gateway, Durganport, WV 48044</Adress>
-          <IconsRow>
-            <SingleIcon>
-              <BiBed />
-              <div>4</div>
-            </SingleIcon>
-            <SingleIcon>
-              <BiBath />
-              <div>2</div>
-            </SingleIcon>
-            <SingleIcon>
-              <BiArea />
-              <div>180 m2</div>
-            </SingleIcon>
-            <FaPaw />
-          </IconsRow>
-        </AddressAndIcons>
-      </PropertyData>
-    </DefaultCard>
+    <CardContainer>
+      <DefaultCard>
+        <PropertyImg>photo</PropertyImg>
+        <PropertyData>
+          <PriceRow>
+            <Price style={{width: "161px"}}>
+              <RiMoneyDollarCircleLine size="30px" color={`${colors.gray.dark}`}/>
+              <text>3,000</text>
+            </Price>
+            <PropertyType>
+              <RiBuildingLine size="30px" color={`${colors.gray.light}`}/>
+              <text>Apartment</text>
+            </PropertyType>
+          </PriceRow>
+          <AddressAndIcons>
+            <Adress>86872 Jacob Gateway, Durganport, WV 48044</Adress>
+            <IconsRow>
+              <SingleIcon>
+                <BiBed size="20px" color={`${colors.gray.regular}`}/>
+                <div>4</div>
+              </SingleIcon>
+              <SingleIcon>
+                <BiBath size="20px" color={`${colors.gray.regular}`}/>
+                <div>2</div>
+              </SingleIcon>
+              <SingleIcon>
+                <BiArea size="20px" color={`${colors.gray.regular}`}/>
+                <div>180 m2</div>
+              </SingleIcon>
+              <FaPaw size="20px" color={`${colors.gray.regular}`}/>
+            </IconsRow>
+          </AddressAndIcons>
+        </PropertyData>
+      </DefaultCard>
+      <ColorBottom />
+    </CardContainer>
   )
 }
