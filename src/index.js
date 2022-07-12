8c15289b36c8432e2ff8a03140eb2462ae1bcaa1
emachipana <emachipana0102@gmail.com>
2022-07-12 16:57:06 -0500

    Apply globals in to index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
+import { Global } from '@emotion/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { reset, global } from './styles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Global styles={reset}/>
+    <Global styles={global}/>
     <App />
   </React.StrictMode>
 );
