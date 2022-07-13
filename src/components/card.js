cdc0391c076066d254aed359d13de844257b76a8
Cesar Martinez <cmarvarc@hotmail.com>
2022-07-12 19:47:09 -0500

    Solved app conflict of rebase

--- src/components/card.js
+++ src/components/card.js
@@ -3,16 +3,27 @@ import { RiMoneyDollarCircleLine } from "react-icons/ri";
 import { RiBuildingLine } from "react-icons/ri";
 import { BiBed } from "react-icons/bi";
 import { BiBath } from "react-icons/bi";
+import { BiArea } from "react-icons/bi";
+import { FaPaw } from "react-icons/fa";
+import { colors } from "../styles/colors";
+import { typography } from "../styles/typography";
+import { fonts } from "../styles/fonts";
 
 const DefaultCard = styled.div`
   width: 300px;
   height: 360px;
 `
 
+const PropertyImg = styled.div`
+  width: 300;
+  height: 200px;
+`
+
 const PropertyData = styled.div`
   display: flex;
   flex-direction: column;
   height: 160px;
+  padding: 8px;
 `
 
 const PriceRow = styled.div`
@@ -22,36 +33,88 @@ const PriceRow = styled.div`
   gap: 10px;
 `
 
+const Price = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 10px;
+  font-family: ${fonts.secundary}; // confirmar si este es Montserrat
+  ${typography.headline.h5};
+`
+
+const PropertyType = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 10px;
+  font-family: ${fonts.primary};
+  color: ${colors.gray.light};
+  ${typography.body.b1}
+`
+
+const AddressAndIcons = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 97px;
+  justify-content: space-between;
+  align-items: flex-start;
+`
+
+const Adress = styled.text`
+  color: ${colors.gray.dark};
+  font-family: ${fonts.secundary};
+  ${typography.subtitle.s1}; // Pendiente corregir el line height: debe ser 24.
+  padding-top: 8px;
+`
+
 const IconsRow = styled.div`
   display: flex;
   flex-direction: row;
   gap: 8px;
 `
 
+const SingleIcon = styled.div`
+  display: flex;  
+  flex-directin: row;
+  gap: 4px;
+`
+
 export function Card() {
 
 
   return(
-    <DefaultCard>
-      <div>photo</div>
+    <DefaultCard style={{border: "2px solid brown"}} >
+      <PropertyImg style={{border: "2px solid brown"}}>photo</PropertyImg>
       <PropertyData>
         <PriceRow>
-          <div>
-            <RiMoneyDollarCircleLine />
+          <Price style={{width: "161px"}}>
+            <RiMoneyDollarCircleLine size="30px" color={`${colors.gray.dark}`}/>
             <text>3,000</text>
-          </div>
-          <div>
-            <RiBuildingLine />
+          </Price>
+          <PropertyType>
+            <RiBuildingLine size="30px" color={`${colors.gray.light}`}/>
             <text>Apartment</text>
-          </div>
+          </PropertyType>
         </PriceRow>
-        <text>Adress</text>
-        <IconsRow>
-          <BiBed />
-
-        </IconsRow>
+        <AddressAndIcons>
+          <Adress>86872 Jacob Gateway, Durganport, WV 48044</Adress>
+          <IconsRow>
+            <SingleIcon>
+              <BiBed />
+              <div>4</div>
+            </SingleIcon>
+            <SingleIcon>
+              <BiBath />
+              <div>2</div>
+            </SingleIcon>
+            <SingleIcon>
+              <BiArea />
+              <div>180 m2</div>
+            </SingleIcon>
+            <FaPaw />
+          </IconsRow>
+        </AddressAndIcons>
       </PropertyData>
-
     </DefaultCard>
   )
 }
