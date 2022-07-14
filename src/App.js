93ba16cb44c58bc4cb042508f4cffb672d098c9d
Diego Manrique <diego.manrique.m@uni.pe>
2022-07-14 10:57:29 -0500

    Clean app file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-import { useState } from "react";
-import Signup from "./pages/signup-page";
-import SignupForm from "./pages/signupform-page";
-
 function App() {
 
-  const [userType, setUserType] = useState(null)
   return (
     <>
-      <SignupForm user={userType} />
-      <Signup setUser={setUserType} />
     </>
   );
 }
