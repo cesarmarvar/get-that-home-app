8e5157146ea78a74e8c21f36bbe384155e409f97
Cesar Martinez <cmarvarc@hotmail.com>
2022-07-14 00:50:28 -0500

    Added footer to landing page and rendered background image on search section. Accomodation is pending

--- src/pages/landing-page.js
+++ src/pages/landing-page.js
@@ -1,28 +1,32 @@
 import styled from "@emotion/styled";
 import Header from "../components/header";
-import { ReactComponent as Ilustration } from "../assets/illustration.svg"
 import { colors, fonts, typography } from "../styles";
 import Button from "../components/Button/button";
 import { LandingSelect } from "../components/landing-select";
 import { PropertyCard } from "../components/card";
 import { ReactComponent as GithubIcon } from "../assets/github.svg";
 import { ReactComponent as LinkedinIcon } from "../assets/linkedin.svg";
+import { ReactComponent as RubyIcon } from "../assets/ruby.svg";
+import { ReactComponent as ReactIcon } from "../assets/react.svg";
 
 
 /* ===== Main section que muestra la barra de busqueda de propiedades =====*/
 
 const Illustration = styled.div`
-width: 100vw;
-height: 600px;
 // position: absolute;
-// z-index: 999px;
+width: 1862.03px;
+height: 600px;
+z-index: 999;
 `
 
 const SearchSection = styled.main`
   display: flex;
   flex-direction: column;
+  padding-top: 60px;
   align-items: center;
   height: 600px;
+  gap: 60px;
+  background-image: url("assets/illustration.svg");
 `
 
 const Introduction = styled.div`
@@ -140,6 +144,24 @@ align-items: center;
 gap: 50px;
 `
 
+/* ===== Seccion 'Meet the team' =====*/
+
+const Footer = styled.div`
+  width: 100%;
+  height: 73px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 16px 120px;
+  gap: 16px;
+  font-family: ${fonts.secundary};
+  background-color: ${colors.background.regular}
+`
+const RowFlex = styled.div`
+display: flex;
+flex-direction: row;
+gap: 12px;
+`
 // style={{border: "2px solid brown"}} ====> debug
 
 export function LandingPage() {
@@ -196,8 +218,12 @@ export function LandingPage() {
               <p style={{textAlign: "center"}}>Cesar Martinez</p>
             </div>
             <IconsContainer>
-              <GithubIcon />
-              <LinkedinIcon />
+              <a style={{cursor: "pointer"}}>
+                <GithubIcon />
+              </a>
+              <a style={{cursor: "pointer"}}>
+                <LinkedinIcon />  
+              </a>            
             </IconsContainer>
           </TeamMemberCard>
           <TeamMemberCard >
@@ -206,8 +232,12 @@ export function LandingPage() {
               <p style={{textAlign: "center"}}>Enmanuel Chipana</p>
             </div>
             <IconsContainer>
-              <GithubIcon />
-              <LinkedinIcon />
+              <a style={{cursor: "pointer"}}>
+                <GithubIcon />
+              </a>
+              <a style={{cursor: "pointer"}}>
+                <LinkedinIcon />  
+              </a>
             </IconsContainer>
           </TeamMemberCard>
           <TeamMemberCard >
@@ -216,8 +246,12 @@ export function LandingPage() {
               <p style={{textAlign: "center"}}>Diego Manrique</p>
             </div>
             <IconsContainer>
-              <GithubIcon />
-              <LinkedinIcon />
+              <a style={{cursor: "pointer"}}>
+                <GithubIcon />
+              </a>              
+              <a style={{cursor: "pointer"}}>
+                <LinkedinIcon />  
+              </a>
             </IconsContainer>
           </TeamMemberCard>
           <TeamMemberCard >
@@ -226,12 +260,33 @@ export function LandingPage() {
               <p style={{textAlign: "center"}}>Luis Zelada</p>
             </div>
             <IconsContainer>
-              <GithubIcon />
-              <LinkedinIcon />
+              <a style={{cursor: "pointer"}}>
+                <GithubIcon />
+              </a>              
+              <a style={{cursor: "pointer"}}>
+                <LinkedinIcon />  
+              </a>
             </IconsContainer>
           </TeamMemberCard>
         </TeamContainer>
       </TeamSection>
+      <Footer>
+        <p>© 2021 - Find That Home</p>
+        <div>
+          <p style={{textAlign: "center"}}>Source Code</p>
+          <RowFlex>
+            <RowFlex>
+              <RubyIcon />
+              <p>Ruby on Rails REST API</p>
+            </RowFlex>
+            <RowFlex>
+              <ReactIcon/>
+              <p>React Responsive SPA</p>
+            </RowFlex>
+          </RowFlex>
+        </div>
+        <p>Codeable - Cohort 3 Final Project</p>
+      </Footer>
     </>
   )
 
