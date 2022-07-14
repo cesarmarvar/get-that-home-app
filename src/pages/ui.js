133a5e3ef4922340a804ca07203ed0ef3ee913af
emachipana <emachipana0102@gmail.com>
2022-07-14 18:26:41 -0500

    ADD: some containers

---
+++ src/pages/ui.js
@@ -0,0 +1,21 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  margin: 0 1rem;
+`;
+
+export const Filters = styled.div`
+  max-width: 1100px;
+  display: flex;
+  gap: 1rem;
+  margin: 2rem auto;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap; 
+`;
+
+export const FilterSection = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
