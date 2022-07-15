fe0545ac7cdd627e04e227e8aaf97dedbbebdefe
emachipana <emachipana0102@gmail.com>
2022-07-14 23:31:54 -0500

    ADD: render modal conditional

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
+import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header/header";
+import LoginForm from "./components/LoginForm";
 import { useAuth } from "./context/auth-context";
 import { LandingPage } from "./pages/landing-page";
 import PropertiesPage from "./pages/properties-page";
 import Signup from "./pages/signup-page";
 import SignupForm from "./pages/signupform-page";
+import { Modal } from "./pages/ui";
 
 function App() {
   const { user } = useAuth();
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
       <Header 
         isAuth={!!user}
         typeUser={user?.user_type}
+        handleOpen={setIsOpen}
       />
+      {
+        isOpen
+        ?
+        <Modal>
+          <LoginForm handleOpen={setIsOpen}/>
+        </Modal>
+        :
+        null
+      }
       <Routes>
         <Route index path="/" element={<LandingPage />} />
         <Route path="/properties" element={<PropertiesPage />}/>
