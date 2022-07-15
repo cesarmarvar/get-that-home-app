df30b78801bd64fe07fa5cc9a1a15e83138902f8
Diego Manrique <diego.manrique.m@uni.pe>
2022-07-15 17:10:56 -0500

    Add prototype buttons

--- src/components/pagination/pagination.js
+++ src/components/pagination/pagination.js
@@ -1,3 +1,22 @@
 function Pagination() {
-  
+
+  function Buttons() {
+    return (
+      <>
+        <button>Atras</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+        <button>Adelante</button>
+      </>
+    )
+  }
+
+  return (
+    <Buttons></Buttons>
+  )
 }
+
+export default Pagination;
