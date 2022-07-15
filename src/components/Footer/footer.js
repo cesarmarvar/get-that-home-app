93cfb93fc2e4bebd48e12345a43f55cbab93cdce
Diego Manrique <diego.manrique.m@uni.pe>
2022-07-15 10:04:55 -0500

    Add frameworks text

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,12 +1,19 @@
 import { ReactComponent as Logo } from "./../../assets/logo.svg";
 import { RiGithubFill } from "react-icons/ri"
+import { DiRuby, DiReact } from "react-icons/di"
 import styled from "@emotion/styled";
 
 const FooterContainer = styled.div`
+  height: 124px;
+  width: 100%;
   display: flex;
   flex-direction: grow;
+  align-items: center;
   justify-content: space-around;
   background: #F5F5F6;
+  position: absolute;
+  bottom: 0;
+  border-top: 1px #BF5F82;
 `
 
 function Footer() {
@@ -27,14 +34,13 @@ function Footer() {
 
   const ContainerMembers = styled.div`
     display: grid;
-    grid-template-columns: 1fr 1fr;
+    grid-template-columns: 180px 180px;
+    grid-template-rows: 1fr 4px 1fr;
   `
-
-
 
   function LogoBox(){
     return (
-      <div>
+      <div style={{ display: "flex", flexDirection: "column", gap: "0.25rem"}}>
         <Logo />
         <Information>© 2022 - Get That Home</Information>
         <Information>Codeable - Cohort 6 Final Project</Information>
@@ -54,7 +60,7 @@ function Footer() {
     }
 
     return (
-      <div>
+      <div style={{ display: "flex", flexDirection: "column", gap: "0.5rem"}}>
         <LittleInfo>Build with ❤ by:</LittleInfo>
         <ContainerMembers>
           <MemberComponent>Enmanuel Chipana</MemberComponent>
@@ -66,11 +72,36 @@ function Footer() {
     )
   }
 
+  function Frameworks() {
+
+    function FrameworksComponent({ children }) {
+      return (
+        <div style={{ display: "flex", flexDirection: "grow", gap: "0.25rem"}}>
+          <Information>{ children }</Information>
+        </div>
+      )
+    }
+
+    return (
+      <div style={{ display: "flex", flexDirection: "column", gap: "0.5rem"}}>
+        <LittleInfo>Source code:</LittleInfo>
+        <div style={{ display: "flex", flexDirection: "column", gap: "0.25rem"}}>
+          <FrameworksComponent>
+            <DiRuby /> Ruby on Rails REST API
+          </FrameworksComponent>
+          <FrameworksComponent>
+            <DiReact /> React Responsive SPA
+          </FrameworksComponent>
+        </div> 
+      </div>
+    )
+  }
 
   return (
     <FooterContainer>
       <LogoBox></LogoBox>
       <Members></Members>
+      <Frameworks></Frameworks>
     </FooterContainer>
   )  
 }
