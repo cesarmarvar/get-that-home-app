1c2d69482aa176813e68921b859e149e4d1dd71c
emachipana <emachipana0102@gmail.com>
2022-07-15 09:26:14 -0500

    ADD: search by address

--- src/pages/properties-page.js
+++ src/pages/properties-page.js
@@ -1,16 +1,46 @@
 import Input from "../components/Input/input";
 import { BsSearch } from "react-icons/bs";
-import { Container, Filters, FilterSection } from "./ui";
+import { CardsContainer, Container, Filters, FilterSection } from "./ui";
 import Filter from "../components/Filter";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useProperties } from "../context/property-context";
+import { PropertyCard } from "../components/Card/card";
 
 function PropertiesPage() {
+  const { properties, searchByAddress } = useProperties();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [currentProps, setCurrentProps] = useState(properties[currentPage]);
+  const [search, setSearch] = useState({
+    isSearching: false,
+    query: ""
+  });
+
   const [filters, setFilters] = useState({
     price: [],
     type: [],
     nums: [],
     more: []
-  })
+  });
+
+  const { query } = search;
+
+  function handleChangePage(value){
+    setCurrentPage(value);
+  }
+
+  function handleQuery(e) {
+    setSearch({...search, query: e.target.value});
+  }
+
+  useEffect(() => {
+    const isSearching = query !== "";
+    setSearch({ query: query, isSearching: isSearching });
+    if(isSearching){
+      setCurrentProps(searchByAddress(query));
+    }else {
+      setCurrentProps(properties[currentPage]);
+    }
+  }, [properties, query, currentPage, searchByAddress]);
 
   return (
     <Container>
@@ -20,6 +50,8 @@ function PropertiesPage() {
           placeholder="Search by address"
           type="text"
           IconL={BsSearch}
+          value={query}
+          onChange={handleQuery}
         />
         <FilterSection>
           <Filter
@@ -45,6 +77,14 @@ function PropertiesPage() {
         </FilterSection>
         <Input />
       </Filters>
+      <CardsContainer>
+        { currentProps.map(prop => (
+            <PropertyCard
+              user="homeseeker"
+              data={prop}
+            />
+        )) }
+      </CardsContainer>
     </Container>
   );
 }
