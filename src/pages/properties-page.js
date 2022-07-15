975f4177e5b929e52fcbf7cb762a5d6df5c403d7
emachipana <emachipana0102@gmail.com>
2022-07-14 19:40:29 -0500

    FIX: remove render of header

--- src/pages/properties-page.js
+++ src/pages/properties-page.js
@@ -1,5 +1,3 @@
-import Header from "../components/Header/header";
-import { useAuth } from "../context/auth-context";
 import Input from "../components/Input/input";
 import { BsSearch } from "react-icons/bs";
 import { Container, Filters, FilterSection } from "./ui";
@@ -7,7 +5,6 @@ import Filter from "../components/Filter";
 import { useState } from "react";
 
 function PropertiesPage() {
-  const { user } = useAuth();
   const [filters, setFilters] = useState({
     price: [],
     type: [],
@@ -16,45 +13,39 @@ function PropertiesPage() {
   })
 
   return (
-    <>
-      <Header 
-        isAuth={!!user}
-        typeUser={user?.user_type}
-      />
-      <Container>
-        <Filters>
-          <Input 
-            id="address"
-            placeholder="Search by address"
-            type="text"
-            IconL={BsSearch}
+    <Container>
+      <Filters>
+        <Input 
+          id="address"
+          placeholder="Search by address"
+          type="text"
+          IconL={BsSearch}
+        />
+        <FilterSection>
+          <Filter
+            filters={filters}
+            setFilters={setFilters}
+            type="price"
           />
-          <FilterSection>
-            <Filter
-              filters={filters}
-              setFilters={setFilters}
-              type="price"
-            />
-            <Filter
-              filters={filters}
-              setFilters={setFilters}
-              type="type"
-            />
-            <Filter
-              filters={filters}
-              setFilters={setFilters}
-              type="nums"
-            />
-            <Filter
-              filters={filters}
-              setFilters={setFilters}
-              type="more"
-            />
-          </FilterSection>
-          <Input />
-        </Filters>
-      </Container>
-    </>
+          <Filter
+            filters={filters}
+            setFilters={setFilters}
+            type="type"
+          />
+          <Filter
+            filters={filters}
+            setFilters={setFilters}
+            type="nums"
+          />
+          <Filter
+            filters={filters}
+            setFilters={setFilters}
+            type="more"
+          />
+        </FilterSection>
+        <Input />
+      </Filters>
+    </Container>
   );
 }
 
