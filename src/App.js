eb224b95ad3194011fe8e3a957a8f02d24e937c0
Lucho Zelada <luisdanger96@gmail.com>
2022-07-15 11:51:00 -0500

    CHORE: to render property form page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import Header from "./components/Header/header";
+import PropertyFormPage from "./pages/propery-form-page";
+
 function App() {
   return (
     <>
+      <Header/>
+      <PropertyFormPage/>
     </>
   );
 }
