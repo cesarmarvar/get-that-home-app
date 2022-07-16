5005af46551dc5539293b52055cc882610f80f54
Diego Manrique <diego.manrique.m@uni.pe>
2022-07-16 11:01:13 -0500

    Add onClick event for buttons

--- src/components/pagination/pagination.js
+++ src/components/pagination/pagination.js
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import Button from "../Button/button";
 
-function Pagination(array) {
+function Pagination({ array, setCurrentPage }) {
 
-  const pages = 6;
+  const pages = array.length;
 
   const [displayButtons, setDisplayButtons] = useState({
     first: 1,
@@ -41,13 +41,12 @@ function Pagination(array) {
     return (
       <div style={{display: "flex"}}>
         { first !== 1 ? <Button onClick={handlerPrev} >Prev</Button> : null }
-        <Button>{ first }</Button>
-        { pages <= first ? null : <Button>{ second }</Button> }
-        { pages <= second ? null : <Button>{ third }</Button> }
-        { pages <= third ? null : <Button>{ fourth }</Button> }
-        { pages <= fourth ? null : <Button>{ fifth }</Button> }
+        <Button onClick={() => setCurrentPage(first)}>{ first }</Button>
+        { pages <= first ? null : <Button onClick={() => setCurrentPage(second)}>{ second }</Button> }
+        { pages <= second ? null : <Button onClick={() => setCurrentPage(third)}>{ third }</Button> }
+        { pages <= third ? null : <Button onClick={() => setCurrentPage(fourth)}>{ fourth }</Button> }
+        { pages <= fourth ? null : <Button onClick={() => setCurrentPage(fifth)}>{ fifth }</Button> }
         { fifth < pages ? <Button onClick={handlerNext} >Next</Button> : null }
-        {/* <Button onClick={handlerNext} >Next</Button> */}
       </div>
     )
   }
